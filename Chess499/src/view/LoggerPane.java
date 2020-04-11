3de7ba32e4e5bff5cda5dc8b421e0f553b37a0fc
lukenewman19 <32115220+lukenewman19@users.noreply.github.com>
2020-04-11 11:45:16 -0500

    Display Score and Possible Moves
    User can now see what moves are available when a piece is selected.

--- Chess499/src/view/LoggerPane.java
+++ Chess499/src/view/LoggerPane.java
@@ -2,12 +2,19 @@ package view;
 
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Paint;
 
 public class LoggerPane extends ScrollPane{
 	
 	private TextArea text;
 	
 	public LoggerPane() {
+		
 		text = new TextArea();
 		text.setWrapText(true);
 		text.setEditable(false);
